6be6df141cfd26860bb7ee41198dc869a3240b9f
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-11 23:09:43 +0600

    transform handler with esbuild

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,39 @@
 import * as esbuild from "esbuild-wasm";
-import { useState } from 'react';
+import { useRef } from "react";
+import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom'
 
 
 
 const App = () => {
-
+    const ref = useRef();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
 
-    const onClick = () => {
-        console.log(input);
-    }
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        });
+        setCode(result.code)
+    };
+   
 
     return <div>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
