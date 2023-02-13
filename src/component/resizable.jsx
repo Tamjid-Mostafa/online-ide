d81adc4aa7d197a90984f7978cb832afb778af58
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-14 03:05:10 +0600

    improved resize performance

--- src/component/resizable.jsx
+++ src/component/resizable.jsx
@@ -1,22 +1,44 @@
 import './resizable.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ResizableBox } from 'react-resizable';
 
 const Resizable = ({ direction, children }) => {
     let resizableProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        let timer;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                setInnerHeight(window.innerHeight)
+                setInnerWidth(window.innerWidth)
+            }, 100)
+
+        };
+        window.addEventListener('resize', listener);
+        return () => {
+            window.removeEventListener('resize', listener);
+        }
+    }, [])
+
+
     if (direction === 'horizontal') {
         resizableProps = {
             className: 'resize-horizontal',
-            minConstraints: [window.innerWidth * 0.2, Infinity],
-            maxConstraints: [window.innerWidth * 0.75, Infinity],
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints: [innerWidth * 0.75, Infinity],
             height: Infinity,
-            width: window.innerWidth * 0.75,
+            width: innerWidth * 0.75,
             resizeHandles: ['e'],
         }
     } else {
         resizableProps = {
             minConstraints: [Infinity, 24],
-            maxConstraints: [Infinity, window.innerHeight * 0.9],
+            maxConstraints: [Infinity, innerHeight * 0.9],
             height: 300,
             width: Infinity,
             resizeHandles: ['s'],
