170e643e073683a0255f398a00095849dd4feef9
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-14 13:07:42 +0600

    Handled asynchronus function

--- src/bundler/index.js
+++ src/bundler/index.js
@@ -13,21 +13,28 @@ const bundle = async (rawCode) => {
             wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
     }
-    const result = await service.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [
-            unpkgPathPlugin(),
-            fetchPlugin(rawCode),
-        ],
-        define: {
-            'process.env.NODE_ENV': '"production"',
-            global: 'window',
-        }
-    });
+    try {
+        const result = await service.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(rawCode),
+            ],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            }
+        });
 
-    return result.outputFiles[0].text;
+        return result.outputFiles[0].text;
+
+    } catch (error) {
+        return error.message;
+    }
+
+   
 }
 
 export default bundle;
--- src/component/Preview.jsx
+++ src/component/Preview.jsx
@@ -22,13 +22,22 @@ const Preview = ({code}) => {
             <body>
                 <div id="root"></div>
                 <script>
+                const handleError = (err) => {
+                    const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                };
+
+                window.addEventListener('message', (event) => {
+                    event.preventDefault();
+                    handleError(event.error);
+                  })
+
                  window.addEventListener('message', (event) => {
                     try {
                         eval(event.data);
                     } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                        console.error(err);
+                        handleError(err)
                     }
                   })
                 </script>
